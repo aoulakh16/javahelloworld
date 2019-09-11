4038ec50e228aeb0cd1295ecb503d852544308d4
Major Singh <aoulakh16@gmail.com>
2019-09-11 02:24:10 -0400

    practice

---
+++ test.java
@@ -0,0 +1,15 @@
+public class test {
+    public static void main(String[] args)
+    {
+        byte b = 4;
+        System.out.println("I have "+b+" bytes of data");
+
+        int apples = 100;
+        System.out.println("We have "+apples+" apples");
+        short apples_2 = 400;
+        System.out.println("we have "+apples_2+" apples");
+        long anything = 19373465322L;
+        System.out.println("just print "+anything+"");
+
+    }
+}
